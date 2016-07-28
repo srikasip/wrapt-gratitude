02d3a79802c22a61deca7b0644537630049ed1d1
Rafe Rosen <rrosen@greenriver.org>
2016-07-28 14:16:45 -0400

    image uploads for products

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'haml-rails'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'carrierwave'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728174342) do
+ActiveRecord::Schema.define(version: 20160728180413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160728174342) do
     t.boolean  "public"
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.string   "image"
   end
 
 end
