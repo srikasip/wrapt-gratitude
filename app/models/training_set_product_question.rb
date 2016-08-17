83025986b1b5351d5d86c9528c74fd808b52a885
Rafe Rosen <rrosen@greenriver.org>
2016-08-17 14:38:12 -0400

    experimental slider, dup validation

--- app/models/training_set_product_question.rb
+++ app/models/training_set_product_question.rb
@@ -8,10 +8,18 @@ class TrainingSetProductQuestion < ApplicationRecord
   has_many :response_impacts, class_name: 'TrainingSetResponseImpact', inverse_of: :training_set_product_question, dependent: :destroy
   accepts_nested_attributes_for :response_impacts
 
+  validate :validate_unique_question_for_product, if: -> {product && survey_question}
+
   # assumes there are no response impacts
   def create_response_impacts
     survey_question.options.each do |option|
       response_impacts.create survey_question_option: option
+    end
+  end
+
+  def validate_unique_question_for_product
+    if training_set.product_questions.where(survey_question_id: survey_question_id, product_id: product_id)
+      errors.add :survey_question, "Already exists for this product in this training set"
     end
   end
 end
