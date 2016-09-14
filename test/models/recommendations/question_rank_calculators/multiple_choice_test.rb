96475613b5927b89108ffbe1529547f923339fc5
Rafe Rosen <rrosen@greenriver.org>
2016-09-14 14:06:16 -0400

    fix tests

--- test/models/recommendations/question_rank_calculators/multiple_choice_test.rb
+++ test/models/recommendations/question_rank_calculators/multiple_choice_test.rb
@@ -4,12 +4,12 @@ module Recommendations
   module QuestionRankCalculators
     class MultipleChoiceTest < ActiveSupport::TestCase
 
-      attr_reader :subject, :product_question, :question_response
+      attr_reader :subject, :gift_question_impact, :question_response
 
       def setup
-        @product_question = object_double(TrainingSetProductQuestion.new)
+        @gift_question_impact = object_double(GiftQuestionImpact.new)
         @question_response = object_double(SurveyQuestionResponse.new)
-        @subject = Recommendations::QuestionRankCalculators::MultipleChoice.new(product_question, question_response)
+        @subject = Recommendations::QuestionRankCalculators::MultipleChoice.new(gift_question_impact, question_response)
       end
 
       def test_question_rank
@@ -17,7 +17,7 @@ module Recommendations
 
         selected_response_impact = object_double(TrainingSetResponseImpact.new, survey_question_option_id: 78, impact: 0.75)
         other_response_impact = object_double(TrainingSetResponseImpact.new, survey_question_option_id: 84, impact: -0.33)
-        allow(product_question).to receive(:response_impacts) { [selected_response_impact, other_response_impact] }
+        allow(gift_question_impact).to receive(:response_impacts) { [selected_response_impact, other_response_impact] }
 
         assert_equal(0.75, subject.question_rank)
       end
@@ -26,7 +26,7 @@ module Recommendations
         allow(question_response).to receive(:survey_question_option_id) { 78 }
 
         other_response_impact = object_double(TrainingSetResponseImpact.new, survey_question_option_id: 84, impact: -0.33)
-        allow(product_question).to receive(:response_impacts) { [other_response_impact] }
+        allow(gift_question_impact).to receive(:response_impacts) { [other_response_impact] }
 
         assert_equal(0, subject.question_rank)
       end
--- test/models/recommendations/question_rank_calculators/range_test.rb
+++ test/models/recommendations/question_rank_calculators/range_test.rb
@@ -4,22 +4,22 @@ module Recommendations
   module QuestionRankCalculators
     class RangeTest < ActiveSupport::TestCase
 
-      attr_reader :subject, :product_question, :question_response
+      attr_reader :subject, :gift_question_impact, :question_response
 
       def setup
-        @product_question = object_double(TrainingSetProductQuestion.new)
+        @gift_question_impact = object_double(GiftQuestionImpact.new)
         @question_response = object_double(SurveyQuestionResponse.new)
-        @subject = Recommendations::QuestionRankCalculators::Range.new(product_question, question_response)
+        @subject = Recommendations::QuestionRankCalculators::Range.new(gift_question_impact, question_response)
       end
 
       def test_question_rank_when_direct_correlation
-        allow(product_question).to receive(:range_impact_direct_correlation?) { true }
+        allow(gift_question_impact).to receive(:range_impact_direct_correlation?) { true }
         allow(question_response).to receive(:range_response) { 0.75 }
         assert_equal(0.75, subject.question_rank)
       end
 
       def test_question_rank_when_indirect_correlation
-        allow(product_question).to receive(:range_impact_direct_correlation?) { false }
+        allow(gift_question_impact).to receive(:range_impact_direct_correlation?) { false }
         allow(question_response).to receive(:range_response) { 0.75 }
         assert_equal(-0.75, subject.question_rank)
       end
