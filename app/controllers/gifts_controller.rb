875d4019f0707e688ed56475f1d66482d1997d87
Rafe Rosen <rrosen@greenriver.org>
2016-09-14 10:38:49 -0400

    cleanup controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,74 +1,48 @@
 class GiftsController < ApplicationController
   before_action :set_gift, only: [:show, :edit, :update, :destroy]
 
-  # GET /gifts
-  # GET /gifts.json
   def index
     @gifts = Gift.all
   end
 
-  # GET /gifts/1
-  # GET /gifts/1.json
   def show
   end
 
-  # GET /gifts/new
   def new
     @gift = Gift.new
   end
 
-  # GET /gifts/1/edit
   def edit
   end
 
-  # POST /gifts
-  # POST /gifts.json
   def create
     @gift = Gift.new(gift_params)
-
-    respond_to do |format|
-      if @gift.save
-        format.html { redirect_to @gift, notice: 'Gift was successfully created.' }
-        format.json { render :show, status: :created, location: @gift }
-      else
-        format.html { render :new }
-        format.json { render json: @gift.errors, status: :unprocessable_entity }
-      end
+    if @gift.save
+      redirect_to @gift, notice: 'Gift was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /gifts/1
-  # PATCH/PUT /gifts/1.json
   def update
-    respond_to do |format|
-      if @gift.update(gift_params)
-        format.html { redirect_to @gift, notice: 'Gift was successfully updated.' }
-        format.json { render :show, status: :ok, location: @gift }
-      else
-        format.html { render :edit }
-        format.json { render json: @gift.errors, status: :unprocessable_entity }
-      end
+    if @gift.update(gift_params)
+      redirect_to @gift, notice: 'Gift was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /gifts/1
-  # DELETE /gifts/1.json
   def destroy
     @gift.destroy
-    respond_to do |format|
-      format.html { redirect_to gifts_url, notice: 'Gift was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to gifts_url, notice: 'Gift was successfully destroyed.'
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_gift
-      @gift = Gift.find(params[:id])
-    end
+  
+  private def set_gift
+    @gift = Gift.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def gift_params
-      params.require(:gift).permit(:name, :description, :selling_price, :cost, :wrapt_sku, :date_available, :date_discontinued)
-    end
+  private def gift_params
+    params.require(:gift).permit(:name, :description, :selling_price, :cost, :wrapt_sku, :date_available, :date_discontinued)
+  end
 end
