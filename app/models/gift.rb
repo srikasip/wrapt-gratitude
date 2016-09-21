62170528cfdcc2ee7a7f47cf8d08ce9a9f74de72
Rafe Rosen <rrosen@greenriver.org>
2016-09-21 12:51:00 -0400

    gift images in index, breadcrumbs [#128796661]

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -4,7 +4,9 @@ class Gift < ApplicationRecord
 
   has_many :gift_question_impacts, dependent: :destroy
   has_many :evaluation_recommendations, dependent: :destroy
+
   has_many :gift_images, -> {order :sort_order}, inverse_of: :gift, dependent: :destroy
+  has_one :primary_gift_image, -> {where primary: true}, class_name: 'GiftImage'
 
   def available?
     date_available >= Date.today && date_discontinued <= Date.today
