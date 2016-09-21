77782bd70a1e3321e4b6ca3c520a7a0fa269fd01
Rafe Rosen <rrosen@greenriver.org>
2016-09-21 13:46:27 -0400

    product search [#128796661]

--- app/controllers/gift_products_controller.rb
+++ app/controllers/gift_products_controller.rb
@@ -2,13 +2,21 @@ class GiftProductsController < ApplicationController
   before_action :set_gift
 
   def index
-    @available_products = Product.where.not(id: @gift.gift_products.select(:product_id)).page(params[:page]).per(25)
+    @available_products = Product
+      .where.not(id: @gift.gift_products.select(:product_id))
+    if params[:q].present?
+      @available_products = @available_products
+        .where("LOWER(title) LIKE ?", "%#{params[:q].downcase}%")
+    end
+    @available_products = @available_products
+      .page(params[:page])
+      .per(25)
   end
 
   def create
     @gift_product = @gift.gift_products.new gift_product_params
     if @gift_product.save
-      redirect_to gift_products_path(@gift, page: params[:page])
+      redirect_to gift_products_path(@gift, context_params)
     else
       render :index
     end
@@ -17,8 +25,13 @@ class GiftProductsController < ApplicationController
   def destroy
     @gift_product = @gift.gift_products.find params[:id]
     @gift_product.destroy
-    redirect_to gift_products_path(@gift, page: params[:page])
+    redirect_to gift_products_path(@gift, context_params)
   end
+
+  def context_params
+    params.permit(:page, :q)
+  end
+  helper_method :context_params
 
   private def set_gift
     @gift = Gift.find params[:gift_id]
