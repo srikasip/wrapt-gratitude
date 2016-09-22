dbe3141089e2eeb87af6602491371baed6d8fa10
Rafe Rosen <rrosen@greenriver.org>
2016-09-22 10:47:42 -0400

    inventory enhancements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921162314) do
+ActiveRecord::Schema.define(version: 20160922140221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +110,6 @@ ActiveRecord::Schema.define(version: 20160921162314) do
     t.string   "title"
     t.text     "description"
     t.decimal  "price",               precision: 10, scale: 2
-    t.boolean  "public"
     t.datetime "created_at",                                               null: false
     t.datetime "updated_at",                                               null: false
     t.string   "wrapt_sku"
