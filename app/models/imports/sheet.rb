4e6d2f463fd12018dccd377e7bf9cfe86d371771
Todd Cesere <tcesere@greenriver.org>
2016-09-30 14:18:10 -0400

    [#131434457] Response Set Import now complains about blank cells for survey_response_name and question_code. In addition, depending on the type, it requires either text_response or slider_response to not be blank, or it requires one of the choice_ columns to be a '1'.

--- app/models/imports/sheet.rb
+++ app/models/imports/sheet.rb
@@ -23,15 +23,27 @@ module Imports
     #
 
     def each
-      (2..@roo_sheet.count).each do |row_number|
-        roo_row = @roo_sheet.row(row_number)
-        row = row_class.new(one_to_one_row_hash(roo_row))
-        row.many_to_one_values = roo_row[many_to_one_column_start..many_to_one_column_limit]
-        yield row
-      end
+      row_number_enumeration.each {|row_number| yield row(row_number)}
+    end
+
+    def map
+      row_number_enumeration.map {|row_number| yield row(row_number)}
     end
 
     ##
+
+    def row_number_enumeration
+      2..@roo_sheet.count
+    end
+
+    def row(row_number)
+      roo_row = @roo_sheet.row(row_number)
+      row = row_class.new(one_to_one_row_hash(roo_row).merge({row_number: row_number}))
+      row.many_to_one_values = roo_row[many_to_one_column_start..many_to_one_column_limit]
+      row
+    end
+
+    ###
 
     def one_to_one_row_hash(roo_row)
       one_to_one_header_order.zip(roo_row).to_h
@@ -45,13 +57,13 @@ module Imports
       one_to_one_header_order.count
     end
 
-    ###
+    ####
 
     def one_to_one_header_order
       @_one_to_one_header_order ||= header_row - row_class::MANY_TO_ONE_HEADERS
     end
 
-    ####
+    #####
 
     def header_row
       @_header_row ||= @roo_sheet.row(1).compact
--- app/models/profile_sets/imports/row.rb
+++ app/models/profile_sets/imports/row.rb
@@ -4,10 +4,25 @@ module ProfileSets
 
       include ActiveModel::Model
 
-      ATTRIBUTE_HEADERS = %w{survey_response_name question_code text_response slider_response}
+      ATTRIBUTE_HEADERS = %w{
+        survey_response_name
+        question_code
+        text_response
+        slider_response
+      }
       MANY_TO_ONE_HEADERS = 10.times.map {|number| "choice_#{number + 1}"}
 
-      attr_accessor *ATTRIBUTE_HEADERS, :numeric_responses
+      #
+
+      REQUIRED_RESPONSES_FOR_TYPE = {
+        'SurveyQuestions::Text' => :text_response,
+        'SurveyQuestions::Range' => :slider_response,
+        'SurveyQuestions::MultipleChoice' => :numeric_responses
+      }
+
+      attr_accessor *ATTRIBUTE_HEADERS, :numeric_responses, :row_number
+
+      validates_presence_of :survey_response_name, :question_code
 
       def many_to_one_values=(values)
         self.numeric_responses = values
