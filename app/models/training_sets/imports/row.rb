f1ed34e11fe60d25650ba6d30f578894c464a85b
Todd Cesere <tcesere@greenriver.org>
2016-10-02 16:31:49 -0400

    [#131434457] Training Set Imports now give a validation error if any of their impacts are missing for existing options.

--- app/models/training_sets/imports/row.rb
+++ app/models/training_sets/imports/row.rb
@@ -4,10 +4,18 @@ module TrainingSets
 
       include ActiveModel::Model
 
-      ATTRIBUTE_HEADERS = %w{gift_sku question_code question_impact slider_impact_direction}
+      ATTRIBUTE_HEADERS = %w{
+        gift_sku
+        question_code
+        question_impact
+        slider_impact_direction
+      }
+
       MANY_TO_ONE_HEADERS = 10.times.map {|number| "choice_#{number + 1}_impact"}
 
-      attr_accessor *ATTRIBUTE_HEADERS, :response_impacts
+      attr_accessor *ATTRIBUTE_HEADERS, :response_impacts, :row_number
+
+      validates_presence_of :gift_sku, :question_code, :question_impact
 
       def many_to_one_values=(values)
         self.response_impacts = values
