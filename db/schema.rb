6732cb7979a5ea624ee9794458619ccad00405d8
Rafe Rosen <rrosen@greenriver.org>
2016-10-26 14:24:02 -0400

    default for profile traits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024134919) do
+ActiveRecord::Schema.define(version: 20161026181950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,29 @@ ActiveRecord::Schema.define(version: 20161024134919) do
     t.index ["survey_id"], name: "index_profile_sets_on_survey_id", using: :btree
   end
 
+  create_table "profile_traits_facets", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_profile_traits_facets_on_topic_id", using: :btree
+  end
+
+  create_table "profile_traits_tags", force: :cascade do |t|
+    t.integer  "facet_id"
+    t.string   "name"
+    t.integer  "position",   default: 0, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["facet_id"], name: "index_profile_traits_tags_on_facet_id", using: :btree
+  end
+
+  create_table "profile_traits_topics", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "survey_question_options", force: :cascade do |t|
     t.integer  "survey_question_id",             null: false
     t.text     "text"
@@ -285,6 +308,8 @@ ActiveRecord::Schema.define(version: 20161024134919) do
   add_foreign_key "products", "vendors"
   add_foreign_key "profile_set_survey_responses", "profile_sets"
   add_foreign_key "profile_sets", "surveys"
+  add_foreign_key "profile_traits_facets", "profile_traits_topics", column: "topic_id"
+  add_foreign_key "profile_traits_tags", "profile_traits_facets", column: "facet_id"
   add_foreign_key "survey_question_response_options", "survey_question_options"
   add_foreign_key "survey_question_response_options", "survey_question_responses"
   add_foreign_key "survey_question_responses", "profile_set_survey_responses"
