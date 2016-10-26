2a9a34a5fb7b18987c5531d7896df22c2f25e695
Rafe Rosen <rrosen@greenriver.org>
2016-10-26 14:36:15 -0400

    tags between -3 and 3

--- app/models/profile_traits/tag.rb
+++ app/models/profile_traits/tag.rb
@@ -1,3 +1,10 @@
 class ProfileTraits::Tag < ApplicationRecord
   belongs_to :facet, class_name: 'ProfileTraits::Facet'
+
+  validate :position, numericality: {greater_than_or_equal_to: -3, less_than_or_equal_to: 3}
+
+  def name_with_position
+    "#{name} (#{position})"
+  end
+
 end
