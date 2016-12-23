62d0a247dbbe9ae5b7b98747a79c740f157df89d
Rafe Rosen <rrosen@greenriver.org>
2016-12-23 12:29:14 -0500

    comment

--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -1,4 +1,6 @@
 class SurveyResponse < ApplicationRecord
+  # TODO note this is just a stub: more functionality will be added in release 8
+
   belongs_to :user
   has_many :gift_recommendations, dependent: :destroy
 end
