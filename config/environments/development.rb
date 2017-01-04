dc7340dc8ab50b486e817f1ee27ad238b8cb9e58
Rafe Rosen <rrosen@greenriver.org>
2017-01-04 11:00:02 -0500

    better errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.active_job.queue_adapter = :sidekiq
+  if ENV['BETTER_ERRORS_EDITOR']
+    BetterErrors.editor = ENV['BETTER_ERRORS_EDITOR'].to_sym
+  end
+  
 end
