3ef33e26a261dd4f17474dde2c52c8306777a05f
dpchck <dman@sover.net>
2017-01-25 11:48:08 -0500

    [#138276071] tweaks

--- app/controllers/admin/evaluation_recommendations_controller.rb
+++ app/controllers/admin/evaluation_recommendations_controller.rb
@@ -5,7 +5,7 @@ module Admin
 
     def show
       @training_set = TrainingSet.find params[:training_set_id] # for breadcrumbs
-      @recommendation = EvaluationRecommendation.find params[:id]
+      @recommendation = EvaluationRecommendation.preload(:survey_response).find_by(id: params[:id])
       @engine = Recommendations::Engine.new(@training_set, @recommendation.survey_response)
       @engine.generate_recommendations
       @question_ranks = @engine.recommendation_question_ranks(@recommendation)
