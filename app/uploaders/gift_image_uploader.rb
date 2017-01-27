06d37fb5cbf19c43650b9739443f276b2406b8d5
Rafe Rosen <rrosen@greenriver.org>
2017-01-27 10:26:05 -0500

    square image thumbnails

--- app/uploaders/gift_image_uploader.rb
+++ app/uploaders/gift_image_uploader.rb
@@ -1,10 +1,10 @@
 class GiftImageUploader < DirectUploader
 
   version :small do
-    process :resize_to_fit => [240, 240]
+    process :resize_to_fit => [480, 480]
   end
   version :large do
-    process :resize_to_fit => [640, 640]
+    process :resize_to_fit => [1280, 1280]
   end
 
   def extension_white_list
