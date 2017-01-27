67891950eb8b556c3de40ed9f7025c0b0d420fd5
Rafe Rosen <rrosen@greenriver.org>
2017-01-27 10:54:09 -0500

    keep original sizes for now

--- app/uploaders/gift_image_uploader.rb
+++ app/uploaders/gift_image_uploader.rb
@@ -1,10 +1,10 @@
 class GiftImageUploader < DirectUploader
 
   version :small do
-    process :resize_to_fit => [480, 480]
+    process :resize_to_fit => [240, 240]
   end
   version :large do
-    process :resize_to_fit => [1280, 1280]
+    process :resize_to_fit => [640, 640]
   end
 
   def extension_white_list
