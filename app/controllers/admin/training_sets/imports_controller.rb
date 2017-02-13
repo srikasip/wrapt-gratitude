cbe9dcb9853ef3942454fa41187c29c1881b5fe5
dpchck <dman@sover.net>
2017-02-13 17:11:45 -0500

    needs testing

--- app/controllers/admin/training_sets/imports_controller.rb
+++ app/controllers/admin/training_sets/imports_controller.rb
@@ -2,40 +2,33 @@ module Admin
   module TrainingSets
     class ImportsController < BaseController
 
-      before_action :set_training_set, only: [:new, :create]
+      before_action :load_training_set, only: [:new, :create]
 
       def new
-        @training_set_import = TrainingSetImport.new(training_set: @training_set)
+        @errors = []
       end
 
       def create
-        @training_set_import = TrainingSetImport.new(permitted_params.merge({training_set: @training_set}))
-        begin
-          if @training_set_import.save_records
-            redirect_to [:admin, @training_set], notice: 'You imported impacts into a training set.'
-          else
-            render :new
-          end
-        rescue ::Imports::Exceptions::PreloadsNotFound => exception
-          @missing_resources_exception = exception
+        @training_set_import = TrainingSetImport.new(@training_set, params[:import_file])
+        @training_set_import.open_file
+        TrainingSet.transaction do
+          @training_set_import.truncate_data
+          @training_set_import.insert_data
+        end
+        if @training_set_import.errors.none?
+          redirect_to [:admin, @training_set], notice: 'You imported impacts into a training set.'
+        else
+          @errors = @training_set_import.errors
           render :new
         end
       end
 
       private
 
-      def set_training_set
+      def load_training_set
         @training_set = TrainingSet.find params[:training_set_id]
       end
 
-      def permitted_params
-        subparams = params.dig(:training_set_import)
-        if subparams
-          subparams.permit(:records_file)
-        else
-          {}
-        end
-      end
     end
   end
 end
--- app/models/training_set_import.rb
+++ app/models/training_set_import.rb
@@ -1,61 +1,94 @@
-class TrainingSetImport < Import
-
-  importable_name :training_set, ExcelImportRecordsFileUploader
-
-  def initialize attrs = {}
-    super
-    @question_scope = training_set.survey.questions
+class TrainingSetImport
+  
+  attr_reader :training_set, :file, :errors
+  
+  def initialize training_set, file
+    @training_set = training_set
+    @file = file
+    @questions = {}
+    @gifts = {}
+    @errors = []
+  end
+  
+  def open_file
+    @xls = Roo::Spreadsheet.open(file)
+    @sheet = @xls.sheet_for(@xls.sheets.first)
+  end
+  
+  def truncate_data
+    training_set.gift_question_impacts.destroy_all
+  end
+  
+  def insert_data
+    # skip header row
+    @row_number = 2
+    @sheet.each_row(offset: 1) do |row|
+      create_question_impact(row)
+      @row_number += 1
+    end
+  end
+  
+  def load_question(code)
+    @questions[code] ||= training_set.survey.questions.preload(:options).find_by(code: code)
   end
 
-  def preload!
-    sheet.cache_columns_scan!(:gift_sku, :question_code)
-    preload_one!(Gift, :gift_sku, :wrapt_sku)
-    preload_one!(@question_scope, :question_code, :code)
-    preload_question_impacts!
+  def load_gift(sku)
+    @gifts[sku] ||= Gift.find_by(wrapt_sku: sku)
   end
+  
+  def create_question_impact(row)
+    gift_sku = row[0].to_s.strip
+    
+    if gift_sku.blank?
+      log_error("gift sku missing")
+      return false
+    end
+    
+    gift = load_gift(gift_sku)
 
-  def record_association_name
-    :gift_question_impacts
+    if gift.blank?
+      log_error("gift sku not found '#{gift_sku}'")
+      return false
+    end
+
+    question_code = row[1].to_s.strip
+    
+    if question_code.blank?
+      log_error("question code missing")
+      return false
+    end
+
+    question = load_question(question_code)
+    
+    if question.blank?
+      log_error("question code not found '#{question_code}'")
+      return false
+    end
+    
+    question_impact = training_set.gift_question_impacts.find_or_initialize_by(gift: gift, survey_question: question)
+
+    question_impact.question_impact = [[row[2]&.value.to_f, 0.0].max, 1.0].min
+    question_impact.range_impact_direct_correlation = row[3]&.value.to_i < 0 ? -1 : 1
+    
+    question_impact.save
+    
+    create_response_impacts(question_impact, row)
+    
+    true
   end
-
-  def required_headers
-    TrainingSets::Imports::Row::ATTRIBUTE_HEADERS + TrainingSets::Imports::Row::MANY_TO_ONE_HEADERS
-  end
-
-  def row_record(row)
-    question = @preloads[@question_scope][row.question_code]
-    gift = @preloads[Gift][row.gift_sku]
-
-    question_impact =
-      @preloads[GiftQuestionImpact][[question.id, gift.id]] ||
-      question.gift_question_impacts.new(gift: gift)
-
-    conversion = TrainingSets::Imports::Conversion.new(row)
-
-    question_impact.question_impact = conversion.question_impact
-
-    question_impact.range_impact_direct_correlation = conversion.range_impact_direct_correlation(question)
-
+  
+  def create_response_impacts(question_impact, row)
     question_impact.response_impacts.destroy_all
-    question_impact.response_impacts = conversion.response_impacts(question)
-
-    question_impact
+    if question_impact.survey_question.is_a?(SurveyQuestions::MultipleChoice)
+      question_impact.survey_question.options.each_with_index do |option, n|
+        impact = [[row[4 + n]&.value.to_f, -1.0].max, 1.0].min
+        question_impact.response_impacts.create(survey_question_option: option, impact: impact)
+      end
+    end
+  end
+  
+  def log_error(message)
+    @errors << "#{@row_number}: #{message}"
   end
 
-  #
-
-  def preload_question_impacts!
-    @preloads ||= {}
-
-    @preloads[GiftQuestionImpact] ||=
-      GiftQuestionImpact.
-        where(
-          training_set: training_set,
-          survey_question: @preloads[@question_scope].values,
-          gift: @preloads[Gift].values
-        ).
-        index_by do |record|
-          [record.survey_question_id, record.gift_id]
-        end
-  end
 end
