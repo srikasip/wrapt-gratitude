c1d93ea5b95b981bde59493d7508cbde8f005050
dpchck <dman@sover.net>
2017-02-13 18:09:53 -0500

    wip

--- app/models/training_set_import.rb
+++ app/models/training_set_import.rb
@@ -12,7 +12,6 @@ class TrainingSetImport
   
   def open_file
     @xls = Roo::Spreadsheet.open(file)
-    @sheet = @xls.sheet_for(@xls.sheets.first)
   end
   
   def truncate_data
@@ -22,8 +21,8 @@ class TrainingSetImport
   def insert_data
     # skip header row
     @row_number = 2
-    @sheet.each_row(offset: 1) do |row|
-      create_question_impact(row)
+    while (@row_number <= @xls.last_row) do
+      create_question_impact
       @row_number += 1
     end
   end
@@ -36,8 +35,8 @@ class TrainingSetImport
     @gifts[sku] ||= Gift.find_by(wrapt_sku: sku)
   end
   
-  def create_question_impact(row)
-    gift_sku = row[0].to_s.strip
+  def create_question_impact
+    gift_sku = column_value(1).to_s.strip
     
     if gift_sku.blank?
       log_error("gift sku missing")
@@ -51,7 +50,7 @@ class TrainingSetImport
       return false
     end
 
-    question_code = row[1].to_s.strip
+    question_code = column_value(2).to_s.strip
     
     if question_code.blank?
       log_error("question code missing")
@@ -67,24 +66,28 @@ class TrainingSetImport
     
     question_impact = training_set.gift_question_impacts.find_or_initialize_by(gift: gift, survey_question: question)
 
-    question_impact.question_impact = [[row[2]&.value.to_f, 0.0].max, 1.0].min
-    question_impact.range_impact_direct_correlation = row[3]&.value.to_i < 0 ? -1 : 1
+    question_impact.question_impact = [[column_value(3).to_f, 0.0].max, 1.0].min
+    question_impact.range_impact_direct_correlation = column_value(4).to_i >= 0
     
     question_impact.save
     
-    create_response_impacts(question_impact, row)
+    create_response_impacts(question_impact)
     
     true
   end
   
-  def create_response_impacts(question_impact, row)
+  def create_response_impacts(question_impact)
     question_impact.response_impacts.destroy_all
     if question_impact.survey_question.is_a?(SurveyQuestions::MultipleChoice)
       question_impact.survey_question.options.each_with_index do |option, n|
-        impact = [[row[4 + n]&.value.to_f, -1.0].max, 1.0].min
+        impact = [[column_value(5 + n).to_f, -1.0].max, 1.0].min
         question_impact.response_impacts.create(survey_question_option: option, impact: impact)
       end
     end
+  end
+  
+  def column_value(column_number)
+    @xls.cell(@row_number, column_number)
   end
   
   def log_error(message)
