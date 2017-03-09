08b2074b735d3526260a640e07bec46c1effe95a
dpchck <dman@sover.net>
2017-03-09 08:39:00 -0500

    get landing page ready for public

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
   root to: 'home#show'
-  get 'science-of-gifting', to: 'static_pages#science_of_gifting', as: :science_of_gifting
-  get 'terms-of-service', to: 'static_pages#terms_of_service', as: :terms_of_service
-  get 'privacy-policy', to: 'static_pages#privacy_policy', as: :privacy_policy
+  
+  if Rails.env.development?
+    get 'science-of-gifting', to: 'static_pages#science_of_gifting', as: :science_of_gifting
+    get 'terms-of-service', to: 'static_pages#terms_of_service', as: :terms_of_service
+    get 'privacy-policy', to: 'static_pages#privacy_policy', as: :privacy_policy
+  end
+  
 
   resources :invitation_requests, only: :create
 
