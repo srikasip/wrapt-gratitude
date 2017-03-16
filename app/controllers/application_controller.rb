9bbe8e705c931403783ec4239278ec553e81abad
dpchck <dman@sover.net>
2017-03-16 11:28:32 -0400

    removed http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :require_login, if: :login_required?
   before_action :set_signed_authentication_cookie
 
-  if Rails.env.production?
-    before_filter :_basic_auth
-
-    def _basic_auth
-      authenticate_or_request_with_http_basic do |user, password|
-        user == 'wrapt' && password == 'greenriver'
-      end
-    end
-  end
-
   # TODO redefine in subclasses as needed
   def login_required?
     true
