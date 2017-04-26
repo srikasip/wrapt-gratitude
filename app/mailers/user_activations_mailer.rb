0a339224a8cf5c8f4937d3a66663dc745829dbde
Julie Strothman <julie@greenriver.com>
2017-04-26 10:08:49 -0400

    add in mailers for non-loop11 users, copy edits

--- app/mailers/user_activations_mailer.rb
+++ app/mailers/user_activations_mailer.rb
@@ -5,8 +5,20 @@ class UserActivationsMailer < ApplicationMailer
     @activation_url = invitation_url(user.activation_token)
 
     mail to: user.email, subject: "Your invitation to try WRAPT" do |format|
-      format.text {render "activation_needed_unmoderated_testing_platform" if @user.unmoderated_testing_platform?}
-      format.html {render "activation_needed_unmoderated_testing_platform" if @user.unmoderated_testing_platform?}
+      format.text do 
+        if @user.unmoderated_testing_platform?
+          render "activation_needed_unmoderated_testing_platform"
+        else
+          render 'activation_needed_email'
+        end
+      end
+      format.html do
+        if @user.unmoderated_testing_platform?
+          render "activation_needed_unmoderated_testing_platform"
+        else
+          render 'activation_needed_email'
+        end
+      end
     end
   end
 
