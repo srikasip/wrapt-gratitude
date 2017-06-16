e9e2877750ddfde865ecf2824a3a4fd8539da845
dpchck <dman@sover.net>
2017-06-16 15:55:54 -0400

    [#147192679] done

--- app/models/reports/mvp1b_user_survey_summary_report.rb
+++ app/models/reports/mvp1b_user_survey_summary_report.rb
@@ -9,9 +9,16 @@ module Reports
     
     def questions
       [
-        [:age, 'Age'],
-        [:gender, 'Gender'],
-        [:zip, 'Zipcode']
+        [:age, 'What age group are you in?'],
+        [:gender, 'What is your gender?'],
+        [:zip, 'What is your zip code?'],
+        [:response_confidence, 'How confident do you feel about your responses to the quiz?'],
+        [:recommendation_confidence, 'How confident do you feel that the software made good gift recommendations?'],
+        [:recommendation_comment, 'Either way, why?'],
+        [:would_use_again, 'How likely would you be to use this site again on your own?'],
+        [:would_tell_friend, 'How likely would you be to use this site for yourself to create a wish list to share with friends & family, or purchase items??'],
+        [:other_services, 'Would you use Wrapt to notify you of her upcoming gift occasions such as her birthday, anniversary or other important dates? Once you take her quiz and get recommendations, we send you reminders or gifts that match her based on your initial quiz - would this interest you?'],
+        [:mailing_address, 'We would like to show our appreciation for your time with a small gift from Wrapt--please tell us your mailing address if you are interested.']
       ]
     end
         
@@ -30,15 +37,20 @@ module Reports
     
     def calculate_question_stats(attr)
       question_stats = []
+      total_response_count = 0
       surveys.group_by do |survey|
         survey[attr].to_s.strip
       end.each do |choice, responses|
         if choice.present?
-          question_stats << [choice, responses.count]
+          total_response_count += responses.count
+          question_stats << {choice: choice, count: responses.count, pct: 0.0}
         end
       end
+      question_stats.each do |qstat|
+        qstat[:pct] = 100.0 * qstat[:count] / total_response_count
+      end
       question_stats.sort! do |a, b|
-        b.last <=> a.last
+        b[:count] <=> a[:count]
       end
       question_stats
     end
