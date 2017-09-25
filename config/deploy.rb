1731410d32249d01b66bde6fe2da90ffd089ed89
Todd Blackman <tdblackman@gmail.com>
2017-09-25 15:30:08 -0400

    [#151332391] SSL Support. Production ready in the comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ end
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push(".well_known", "certificates", "challenge", "key", 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
