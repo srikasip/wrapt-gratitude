28a2491220f558a692633fb340c5b94a21fd82f0
Todd Blackman <tdblackman@gmail.com>
2017-10-10 11:31:28 -0400

    Shipping webhook now results in email to customer once shipped and updates order statuses

--- app/models/ecommerce/customer_purchase/shipping_service.rb
+++ app/models/ecommerce/customer_purchase/shipping_service.rb
@@ -1,5 +1,6 @@
 class CustomerPurchase::ShippingService
   include ActionView::Helpers::NumberHelper
+  include OrderStatuses
 
   attr_accessor :cart_id, :customer_order
 
@@ -119,7 +120,7 @@ class CustomerPurchase::ShippingService
   end
 
   def shipping_choices_for_view
-    # TOOD: if both choices are same price, don't give a choice
+    # TODO: if both choices are same price, don't give a choice
     shipping_choices.values
   end
 
@@ -211,8 +212,18 @@ class CustomerPurchase::ShippingService
       tracking_payload: data
     })
 
-    raise "WIP"
-    _update_the_customer_order_and_or_purchase_order_statuses!
+    if shipping_label.shipped?
+      purchase_order = shipping_label.purchase_order
+      purchase_order.status = SHIPPED
+      purchase_order.save!
+
+      if customer_order.purchase_orders.all?(&:shipped?)
+        customer_order.status = SHIPPED
+        customer_order.save!
+      end
+
+      CustomerOrderMailer.order_shipped(purchase_order.id).deliver_later
+    end
   end
 
   private
