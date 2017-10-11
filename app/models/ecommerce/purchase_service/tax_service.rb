a6ac5e59bee10d5627828ecc6e43e950fba9635b
Todd Blackman <tdblackman@gmail.com>
2017-10-11 15:51:00 -0400

    Tax service beginnings. cancellations probably just a single method

---
+++ app/models/ecommerce/purchase_service/tax_service.rb
@@ -0,0 +1,24 @@
+class PurchaseService::TaxService
+  def initialize(cart_id:, customer_order:nil)
+    self.cart_id        = cart_id
+    self.customer_order = customer_order || CustomerOrder.find_by(cart_id: self.cart_id)
+  end
+
+  def estimated_tax_in_cents
+    0.0
+  end
+
+  def estimated_tax_in_dollars
+    0.0
+  end
+
+  def submit_estimated_tax!
+    :no_op
+  end
+
+  private
+
+  def _safely
+    yield
+  end
+end
