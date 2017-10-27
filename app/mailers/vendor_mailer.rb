073e3c4bf41f061b7d477f1299478fe0daf22337
Todd Blackman <tdblackman@gmail.com>
2017-10-27 10:48:00 -0400

    [#152350488] Todd gets BCC on ecommerce emails for now

--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -1,4 +1,8 @@
 class VendorMailer < ApplicationMailer
+  if ENV['ECOMMERCE_BCC']
+    default bcc: ENV['ECOMMERCE_BCC'].split(';')
+  end
+
   def acknowledge_order_request(purchase_order_id)
     @purchase_order = PurchaseOrder.find(purchase_order_id)
     @vendor = @purchase_order.vendor
