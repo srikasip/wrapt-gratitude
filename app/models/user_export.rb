f15738ecbae29d0e205c8f8685c4949a54f6dbc3
Todd Blackman <tdblackman@gmail.com>
2017-11-02 12:07:20 -0400

    [#152507323][#152316523] Quick and dirty user export

---
+++ app/models/user_export.rb
@@ -0,0 +1,16 @@
+require 'csv'
+class UserExport
+  def self.csv
+    CSV.generate do |csv|
+      csv << ['First Name', 'Last Name', 'Email Address', 'Newsletter Sign-up']
+      User.where("email not like '%greenriver%'").find_each do |user|
+        csv << [
+          user.first_name,
+          user.last_name,
+          user.email,
+          (user.wants_newsletter ? 'Yes' : 'No')
+        ]
+      end
+    end
+  end
+end
