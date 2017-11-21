4d24c6f46c8b1e62e83f5817af543e6a7bf33708
Megan Eborn <mneborn@gmail.com>
2017-11-20 20:43:59 -0700

    [#153032676] add encouragement message to shipping giftee name

--- app/helpers/ecommerce/checkout_helper.rb
+++ app/helpers/ecommerce/checkout_helper.rb
@@ -7,4 +7,15 @@ module Ecommerce::CheckoutHelper
   def show_checkout_nav?
     action_name != 'finalize'
   end
+
+  def encouragement_message(customer_order)
+    wrapt_text = ['Your gift is almost on it’s way!']
+    if customer_order.gift_wrapt? && customer_order.include_note?
+      wrapt_text.push('Wrapped beautifully, with a handwritten card.')
+    elsif customer_order.gift_wrapt?
+      wrapt_text.push('Wrapped beautifully.')
+    end
+    wrapt_text.push('Good job, you.')
+    wrapt_text.join(' ')
+  end
 end
