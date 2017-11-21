d89544012d253643b03115200982df7b8520f8d1
Megan Eborn <mneborn@gmail.com>
2017-11-20 19:32:46 -0700

    [#153025994] open gift basket when a gift is added

--- app/controllers/gift_selections_controller.rb
+++ app/controllers/gift_selections_controller.rb
@@ -7,7 +7,7 @@ class GiftSelectionsController < ApplicationController
   def create
     @gift_selection = @profile.gift_selections.new gift_selection_params
     if @gift_selection.save
-      broadcast_updated_gift_selections
+      broadcast_updated_gift_selections(true)
     end
     head :ok
   end
@@ -15,7 +15,7 @@ class GiftSelectionsController < ApplicationController
   def destroy
     @gift_selection = @profile.gift_selections.find params[:id]
     @gift_selection.destroy
-    broadcast_updated_gift_selections
+    broadcast_updated_gift_selections(true)
     head :ok
   end
 
@@ -29,14 +29,15 @@ class GiftSelectionsController < ApplicationController
     @profile = current_user.owned_profiles.find params[:giftee_id]
   end
 
-  private def broadcast_updated_gift_selections
+  private def broadcast_updated_gift_selections(open_gift_basket)
     GiftSelectionsChannel.broadcast_to @profile,
       gift_selections_html: render_gift_selections,
       gift_basket_count: @profile.gift_selections.count,
       updated_gift_id: @gift_selection.gift_id,
       add_button_html: render_add_button_html,
       num_gifts_words: pluralize(@profile.gift_selections.count, 'item'),
-      subtotal: number_to_currency(@profile.selling_price_total)
+      subtotal: number_to_currency(@profile.selling_price_total),
+      open: open_gift_basket
   end
 
   private def render_gift_selections
