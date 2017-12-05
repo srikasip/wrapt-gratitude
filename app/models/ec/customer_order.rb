80ece081930f139e8dc6c9502fe6b429316e0048
Todd Blackman <tdblackman@gmail.com>
2017-12-05 11:54:31 -0500

    adjust note showing for vendors so as not to break the note adding logic during checkout

--- app/models/ec/customer_order.rb
+++ app/models/ec/customer_order.rb
@@ -67,7 +67,7 @@ module Ec
     define_method(:bad_tax_transaction?) { self.tax_transactions.present? && !self.tax_transactions&.all?(&:success?) }
     define_method(:bad_labels?) { !self.shipping_labels.all?(&:success?) }
 
-    define_method(:include_note?) { read_attribute(:include_note) && self.note_content.present? || self.note_envelope_text.present? }
+    define_method(:valid_note?) { include_note? && self.note_content.present? || self.note_envelope_text.present? }
 
     def non_cancelled_line_items
       line_items.reject do |line_item|
