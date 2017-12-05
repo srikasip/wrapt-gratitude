8492b9038573fc6c1b8049080420c00c29a14090
Todd Blackman <tdblackman@gmail.com>
2017-12-05 13:49:39 -0500

    [HOTFIX] vendor changing of parcel bugfix

--- app/models/ec/purchase_service/shipping_service.rb
+++ app/models/ec/purchase_service/shipping_service.rb
@@ -294,7 +294,7 @@ module Ec
         if po.forced_shipping_service_level.present?
           shipping_label.carrier = po.forced_shipping_carrier.name
           shipping_label.service_level = po.forced_shipping_service_level.name
-          shipping_label.shippo_object_id = po.rates.find { |r| r.dig('servicelevel', 'name') == po.forced_shipping_service_level.name }[:object_id]
+          shipping_label.shippo_object_id = shipment.rates.find { |r| r.dig('servicelevel', 'name') == po.forced_shipping_service_level.name }['object_id']
         else
           shipping_label.shippo_object_id = rate['object_id']
           shipping_label.carrier = rate['provider']
