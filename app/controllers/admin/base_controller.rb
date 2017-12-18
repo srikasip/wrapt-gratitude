d0744d18db2176e3defd790a575d547b95e169ae
Todd Blackman <tdblackman@gmail.com>
2017-12-18 13:28:28 -0500

    [HOTTEST HOTFIX] Prevent non-admin logged-in users from visiting admin pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,11 +2,8 @@ module Admin
   class BaseController < ApplicationController
 
     layout 'admin'
-    
-    # turn back on when we have real admin accounts @rrosen - 9/7/2016
-    # before_action :authenticate_user!, unless: :devise_controller?
-    # before_action :require_admin!, unless: :devise_controller?
 
+    before_action :require_admin!
 
     def admin_login_required?
       true
@@ -18,6 +15,6 @@ module Admin
         redirect_to root_path
       end
     end
-    
+
   end
 end
