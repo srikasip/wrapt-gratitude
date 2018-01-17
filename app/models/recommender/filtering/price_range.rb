672fb9535b2f8108e37389170a80456009662c9b
dpchck <dman@sover.net>
2018-01-17 14:48:11 -0500

    [#154212183] wip

--- app/models/recommender/filtering/price_range.rb
+++ app/models/recommender/filtering/price_range.rb
@@ -3,23 +3,30 @@ module Recommender
     class PriceRange < Base
       include ActionView::Helpers::NumberHelper
       
-      attr_reader :min_price, :max_price
+      attr_reader :ranges
       
       def load_params
-        @min_price = nil
-        @max_price = nil
-        # we only support one price_range filter
-        params = find_params('price_range').first
-        if params.present?
-          @min_price = params['min_price'].to_f if params.has_key?('min_price')
-          @max_price = params['max_price'].to_f if params.has_key?('max_price')
-        end
+        @ranges = find_params('price_range') || []
+        @ranges = @ranges.select{|r| validate_range(r)}
       end
       
       def description
         if !valid?
           ""
-        elsif min_price.present? && max_price.present?
+        else
+          ranges.map{|r| describe_range(r)}.join(' or ')
+        end
+      end
+      
+      def valid?
+        ranges.any?
+      end
+      
+      def describe_range(range)
+        min_price = range['min_price']
+        max_price = range['max_price']
+        
+        if min_price.present? && max_price.present?
           "price is between #{number_to_currency(min_price)} and #{number_to_currency(max_price)}"
         elsif min_price.present?
           "price is greater than or equal to #{number_to_currency(min_price)}"
@@ -28,32 +35,55 @@ module Recommender
         end
       end
       
-      def valid?
+      def validate_range(range)
+        min_price = range['min_price']
+        min_price = min_price.to_f if min_price.present?
+          
+        max_price = range['max_price']
+        max_price = max_price.to_f if max_price.present?
+          
         if min_price.present? && min_price < 0
-          false
+          return false
         elsif max_price.present? && max_price <= 0
-          false
+          return false
         elsif max_price.present? && min_price.present?
-          max_price > min_price
+          return max_price > min_price
         elsif max_price.blank? && min_price.blank?
-          false
-        else
-          true
+          return false
         end
+        
+        range['min_price'] = min_price if min_price.present?
+        range['max_price'] = max_price if max_price.present?
+        
+        return true
       end
       
       def gift_scope
         return nil unless valid?
-        cgf = CalculatedGiftField.arel_table
+        
         scope = Gift.joins(:calculated_gift_field)
-        if min_price.present? && max_price.present?
-          scope = scope.where(cgf[:price].between(min_price..max_price))
-        elsif min_price.present?
-          scope = scope.where(cgf[:price].gteq(min_price))
-        elsif max_price.present?
-          scope = scope.where(cgf[:price].lteq(max_price))
+
+        criteria = arel_criteria_for_range(ranges.first)
+        ranges[1..-1].each do |range|
+          criteria.or(arel_criteria_for_range(ranges))
         end
-        scope.select(:id)
+        
+        scope.select(:id).where(criteria)
+      end
+      
+      def arel_criteria_for_range(range)
+        min_price = range['min_price']
+        max_price = range['max_price']
+        
+        cgf = CalculatedGiftField.arel_table
+        
+        if min_price.present? && max_price.present?
+          cgf[:price].between(min_price..max_price)
+        elsif min_price.present?
+          cgf[:price].gteq(min_price)
+        elsif max_price.present?
+          cgf[:price].lteq(max_price)
+        end
       end
     end
   end
