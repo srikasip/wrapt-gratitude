a87773d8f9845329f852bf0241eaea6702b26d57
Todd Blackman <tdblackman@gmail.com>
2018-01-23 13:41:53 -0500

    [#154483558] Cloning with other option type of option should now keep the other option at the end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112194803) do
+ActiveRecord::Schema.define(version: 20180123184357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
