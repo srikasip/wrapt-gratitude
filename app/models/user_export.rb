b13e0377f427ca72dc40eca35275735a9de93d31
dpchck <dman@sover.net>
2018-01-30 15:30:14 -0500

    [#154640886] added recommendation counts

--- app/models/user_export.rb
+++ app/models/user_export.rb
@@ -1,14 +1,16 @@
 require 'csv'
 class UserExport
   def self.csv
+    notification_counts = Profile.with_notifications.group(:owner_id).count(:id)
     CSV.generate do |csv|
-      csv << ['First Name', 'Last Name', 'Email Address', 'Newsletter Sign-up']
+      csv << ['First Name', 'Last Name', 'Email Address', 'Newsletter Sign-up', 'Notifications']
       User.where("email not like '%greenriver%'").order('email').each do |user|
         csv << [
           user.first_name,
           user.last_name,
           user.email,
-          (user.wants_newsletter ? 'Yes' : 'No')
+          (user.wants_newsletter ? 'Yes' : 'No'),
+          (notification_counts[user.id].to_i)
         ]
       end
     end
