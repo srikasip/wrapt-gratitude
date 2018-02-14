516a9efaea811e3f17bb8f8ff559412e970e5fd6
dpchck <dman@sover.net>
2018-02-14 14:21:05 -0500

    fix invite logic

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,7 +3,8 @@ class InvitationsController < ApplicationController
   def show
     user = User.load_from_activation_token(params[:id])
     if user.present?
-      user.update_attribute(:activation_state, 'active')
+      user.activate!
+      auto_login(user)
     else
       flash.alert = 'Sorry that link is not valid'
     end
