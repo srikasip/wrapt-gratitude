30d188c739cee144ed07e4accf6bfcbebc845a06
Todd Blackman <tdblackman@gmail.com>
2018-03-23 15:12:41 -0400

    I see actioncable errors, and maybe this will help

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,7 @@ end
 Sidekiq.configure_server do |config|
   # Uploaded images need to be processed faster than the default 15 second interval allows for.
   config.average_scheduled_poll_interval = 1
+  config.redis = { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/0"} }
 end
 
 Sidekiq.default_worker_options = {'backtrace' => true}
